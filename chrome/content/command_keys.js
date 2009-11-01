b59dc72c99e7f36b4cf20f68829f2fcf291338af
David Logie <davidlogie@mac.com>
2009-11-01 12:56:20 +0000

    Simplified 'ck_load_bookmark' function.

--- chrome/content/command_keys.js
+++ chrome/content/command_keys.js
@@ -1,44 +1,43 @@
 function ck_load_bookmark(id)
 {
-	var bookmarks_bar = document.getElementById("bookmarksBarContent");
-	if (bookmarks_bar.hasChildNodes())
-	{
-		var bookmarks = bookmarks_bar.childNodes;
-		if (id <= bookmarks.length)
-		{	
-			var position = -1;
-			var count = 0;
-			do
-			{
-				position++;
-				if (bookmarks.item(position).nodeName != "toolbarseparator")
-				{
-					count++;
-				}
-			}
-			while (count != id && position + 1 <= bookmarks.length);
-			if (bookmarks.item(position).nodeName != "toolbarseparator")
-			{
-				bookmarks.item(position).doCommand();
-			}
-		}
-	}
+    id = id - 1;
+    var bookmarks_bar = document.getElementById("bookmarksBarContent");
+    if (bookmarks_bar.hasChildNodes())
+    {
+        var bookmarks = bookmarks_bar.childNodes;
+        var count = 0;
+        if (id <= bookmarks.length)
+        {
+            while (count <= id)
+            {
+                item = bookmarks.item(count);
+                if (item.nodeName != "toolbarseparator")
+                {
+                    if (count == id)
+                    {
+                        item.doCommand();
+                    }
+                    count++;
+                }
+            }
+        }
+    }
 }
 function ck_set_keys()
 {
     var keys_list = document.getElementById("mainKeyset");
     var keyset = document.createElement("keyset");
     keyset.setAttribute('id', 'ckKeyset');
-	var key = null;	
-	for (var i = 1; i <= 9; i++)
-	{
-		key = document.createElement("key");
-		key.setAttribute("id", "ck_load_bookmark_" + i);
-		key.setAttribute("key", i);
-		key.setAttribute("oncommand", "ck_load_bookmark(" + i + ");");
-		key.setAttribute("modifiers", "accel");
-		keyset.appendChild(key);
-	}
-	keys_list.appendChild(keyset);
+    var key = null;
+    for (var i = 1; i <= 9; i++)
+    {
+        key = document.createElement("key");
+        key.setAttribute("id", "ck_load_bookmark_" + i);
+        key.setAttribute("key", i);
+        key.setAttribute("oncommand", "ck_load_bookmark(" + i + ");");
+        key.setAttribute("modifiers", "accel");
+        keyset.appendChild(key);
+    }
+    keys_list.appendChild(keyset);
 }
 ck_set_keys();
